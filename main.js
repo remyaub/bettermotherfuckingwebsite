2868c10ecf736f3dda90a4800eb9262f3c554744
remyaub <45595989+remyaub@users.noreply.github.com>
2018-12-11 19:51:13 +0100

    add JS

---
+++ main.js
@@ -0,0 +1,6 @@
+ok
+setTimeout(function(){ 
+  if (confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please")) {
+    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank'); 
+  } 
+}, 10000);
