1fa01d30d4fe097e055e27693450e0e0f57e3cde
remyaub <45595989+remyaub@users.noreply.github.com>
2018-12-11 20:23:54 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-ok
 setTimeout(function(){ 
   if (confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please")) {
     window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank'); 
