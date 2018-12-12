42e77ce2da25c13ff7658e08c8d4352a9aa82c92
remyaub <centratobey@gmai.com>
2018-12-12 10:53:51 +0100

    name changed to index

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+setTimeout(function(){ 
+  if (confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please")) {
+    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank'); 
+  } 
+}, 10000);
+
+function myFunction() {
+    document.body.style.backgroundColor = "pink";
+
+}
