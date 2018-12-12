f41376791d690b0448f450b3144c3143bbbfb3a2
remyaub <centratobey@gmai.com>
2018-12-12 15:04:19 +0100

    new js

--- scripts/main.js
+++ scripts/main.js
@@ -6,22 +6,37 @@ setTimeout(function(){
 
 function myFunction() {
     document.body.style.backgroundColor = "pink";
-    
+    document.body.style.fontFamily = "Arial,Charcoal,sans-serif";
+
     var allH1 = document.getElementsByTagName("h1");
 for(var i = 0; i < allH1.length; i++) {
    allH1[i].style.color = "green";
+   	}
 
- var allH2 = document.getElementsByTagName("h2");
+ 	var allH2 = document.getElementsByTagName("h2");
 for(var i = 0; i < allH2.length; i++) {
    allH2[i].style.color = "green";
+   	}
 
-var allH3 = document.getElementsByTagName("h3");
+	var allH3 = document.getElementsByTagName("h3");
 for(var i = 0; i < allH3.length; i++) {
    allH3[i].style.color = "green";
-
+   	}
 
     var allP = document.getElementsByTagName("p");
 for(var i = 0; i < allP.length; i++) {
-   allP[i].style.color = "blue";  
+   allP[i].style.color = "blue";
+  	}
 
-}}}}}
+    var allLI = document.getElementsByTagName("li");
+for(var i = 0; i < allLI.length; i++) {
+   allLI[i].style.color = "blue";
+	}
+
+   var allBLOCKQUOTE = document.getElementsByTagName("blockquote");
+for(var i = 0; i < allBLOCKQUOTE.length; i++) {
+   allBLOCKQUOTE[i].style.color = "blue";
+	}
+
+}
+
