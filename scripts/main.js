8e228e5b74c3a951db624ac730f6ce4cc352a4d5
remyaub <45595989+remyaub@users.noreply.github.com>
2019-01-03 13:25:26 +0000

    V good

--- scripts/main.js
+++ scripts/main.js
@@ -8,60 +8,44 @@ function myFunction() {
     document.body.style.backgroundColor = "pink";
     document.body.style.fontFamily = "Arial,Charcoal,sans-serif";
 
-    var allH1 = document.getElementsByTagName("h1");
-for(var i = 0; i < allH1.length; i++) {
-   allH1[i].style.color = "green";
-   	}
 
     var allH1 = document.getElementsByTagName("h1");
-for(var i = 0; i < allH1.length; i++) {
-   allH1[i].style.fontFamily = "Comic Sans";
-   	} 
-
- 	var allH2 = document.getElementsByTagName("h2");
-for(var i = 0; i < allH2.length; i++) {
-   allH2[i].style.color = "green";
+    for(var i = 0; i < allH1.length; i++) {
+      allH1[i].style.color = "green";
+      allH1[i].style.fontFamily = "Comic Sans";
    	}
 
-	var allH2 = document.getElementsByTagName("h2");
-for(var i = 0; i < allH2.length; i++) {
-   allH2[i].style.fontFamily = "Comic Sans";
+
+   	var allH2 = document.getElementsByTagName("h2");
+    for(var i = 0; i < allH2.length; i++) {
+      allH2[i].style.color = "green";
+      allH2[i].style.fontFamily = "Comic Sans";
    	}
 
-	var allH3 = document.getElementsByTagName("h3");
-for(var i = 0; i < allH3.length; i++) {
-   allH3[i].style.color = "green";
-   	}
 
-	var allH3 = document.getElementsByTagName("h3");
-for(var i = 0; i < allH3.length; i++) {
-   allH3[i].style.fontFamily = "Comic Sans";
+    var allH3 = document.getElementsByTagName("h3");
+    for(var i = 0; i < allH3.length; i++) {
+      allH3[i].style.color = "green";
+      allH3[i].style.fontFamily = "Comic Sans"
    	}
 
     var allP = document.getElementsByTagName("p");
-for(var i = 0; i < allP.length; i++) {
-   allP[i].style.color = "blue";
+    for(var i = 0; i < allP.length; i++) {
+      allP[i].style.color = "blue";
+      allP[i].style.fontFamily = "papyrus"
   	}
 
-   var allP = document.getElementsByTagName("p");
-for(var i = 0; i < allP.length; i++) {
-   allP[i].style.fontFamily = "papyrus";
-  	}
 
     var allLI = document.getElementsByTagName("li");
-for(var i = 0; i < allLI.length; i++) {
-   allLI[i].style.color = "blue";
+    for(var i = 0; i < allLI.length; i++) {
+      allLI[i].style.color = "blue";
+      allLI[i].style.fontFamily = "papyrus"
+
 	 }
 
-   var allLI = document.getElementsByTagName("li");
-for(var i = 0; i < allLI.length; i++) {
-   allLI[i].style.fontFamily = "papyrus";
-   }
-
    var allBLOCKQUOTE = document.getElementsByTagName("blockquote");
-for(var i = 0; i < allBLOCKQUOTE.length; i++) {
+   for(var i = 0; i < allBLOCKQUOTE.length; i++) {
    allBLOCKQUOTE[i].style.color = "blue";
+   allBLOCKQUOTE[i].style.fontFamily = "papyrus";
 	}
-
-}
 
